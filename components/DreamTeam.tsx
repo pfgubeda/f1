f369cf366e53b60e87e775763c140f4946011215
PCarretero28 <120803089+PCarretero28@users.noreply.github.com>
2023-09-11 19:42:03 +0200

    2 drivers and 1 constructor with button

--- components/DreamTeam.tsx
+++ components/DreamTeam.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, ImageSourcePropType } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageSourcePropType, Alert, TouchableOpacity } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import * as Resources from './Resources';
 
@@ -8,7 +8,8 @@ interface DreamTeamProps {
 }
 
 interface DreamTeamState {
-  selectedDriver: string | null;
+  selectedDriver1: string | null;
+  selectedDriver2: string | null;
   selectedConstructor: string | null;
 }
 
@@ -20,7 +21,8 @@ export default class DreamTeam extends Component<
     super(props);
 
     this.state = {
-      selectedDriver: null,
+      selectedDriver1: null,
+      selectedDriver2: null,
       selectedConstructor: null,
     };
 
@@ -28,6 +30,24 @@ export default class DreamTeam extends Component<
       headerShown: true,
     });
   }
+
+  handleDriver1Change = (itemValue: string | null) => {
+    // Evitar seleccionar el mismo piloto en Driver1 y Driver2
+    if (itemValue !== this.state.selectedDriver2) {
+      this.setState({ selectedDriver1: itemValue });
+    }else {
+      Alert.alert('Warning!', 'This driver has been already chosen');
+    }
+  };
+
+  handleDriver2Change = (itemValue: string | null) => {
+    // Evitar seleccionar el mismo piloto en Driver2 y Driver1
+    if (itemValue !== this.state.selectedDriver1) {
+      this.setState({ selectedDriver2: itemValue });
+    }else {
+      Alert.alert('Warning!', 'This driver has been already chosen');
+    }
+  };
 
   render() {
     const driverOptions = [
@@ -72,65 +92,101 @@ export default class DreamTeam extends Component<
       <View style={styles.container}>
         <Text style={styles.title}>Choose your favorites</Text>
 
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Driver</Text>
-          <Picker
-          style={styles.picker}
-            selectedValue={this.state.selectedDriver}
-            onValueChange={(itemValue) =>
-              this.setState({ selectedDriver: itemValue })
-            }
-          >
-            <Picker.Item label="Select Driver" value={null} />
-            {driverOptions.map((driver, index) => (
-              <Picker.Item
-                key={index}
-                label={driver.replace(/_/g, ' ')}
-                value={driver}
+        {/* Fila conductores */}
+        <View style={styles.row}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Driver 1</Text>
+            <Picker
+              style={styles.picker}
+              selectedValue={this.state.selectedDriver1}
+              onValueChange={this.handleDriver1Change}
+            >
+              <Picker.Item label="Select Driver" value={null} />
+              {driverOptions.map((driver, index) => (
+                <Picker.Item
+                  key={index}
+                  label={driver.replace(/_/g, ' ')}
+                  value={driver}
+                />
+              ))}
+            </Picker>
+            {this.state.selectedDriver1 && (
+              <Image
+                source={(Resources as Record<string, ImageSourcePropType>)[
+                  this.state.selectedDriver1
+                ]}
+                style={styles.image}
               />
-            ))}
-          </Picker>
-          {this.state.selectedDriver && (
-            <Image
-              source={(Resources as Record<string, ImageSourcePropType>)[
-                this.state.selectedDriver
-              ]}
-              style={styles.image}
-            />
-          )}
+            )}
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Driver 2</Text>
+            <Picker
+              style={styles.picker}
+              selectedValue={this.state.selectedDriver2}
+              onValueChange={this.handleDriver2Change}
+            >
+              <Picker.Item label="Select Driver" value={null} />
+              {driverOptions.map((driver, index) => (
+                <Picker.Item
+                  key={index}
+                  label={driver.replace(/_/g, ' ')}
+                  value={driver}
+                />
+              ))}
+            </Picker>
+            {this.state.selectedDriver2 && (
+              <Image
+                source={(Resources as Record<string, ImageSourcePropType>)[
+                  this.state.selectedDriver2
+                ]}
+                style={styles.image}
+              />
+            )}
+          </View>
         </View>
 
-        <View style={styles.inputContainer}>
-          <Text style={styles.label}>Constructor</Text>
-          <Picker
-          style={styles.picker}
-            selectedValue={this.state.selectedConstructor}
-            onValueChange={(itemValue) =>
-              this.setState({ selectedConstructor: itemValue })
-            }
-          >
-            <Picker.Item label="Select Constructor" value={null} />
-            {constructorOptions.map((constructor, index) => (
-              <Picker.Item
-                key={index}
-                label={constructor.replace(/_/g, ' ')}
-                value={constructor}
+        {/* Fila constructor */}
+        <View style={styles.row}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Constructor</Text>
+            <Picker
+              style={styles.picker}
+              selectedValue={this.state.selectedConstructor}
+              onValueChange={(itemValue) =>
+                this.setState({ selectedConstructor: itemValue })
+              }
+            >
+              <Picker.Item label="Select Constructor" value={null} />
+              {constructorOptions.map((constructor, index) => (
+                <Picker.Item
+                  key={index}
+                  label={constructor.replace(/_/g, ' ')}
+                  value={constructor}
+                />
+              ))}
+            </Picker>
+            {this.state.selectedConstructor && (
+              <Image
+                source={(Resources as Record<string, ImageSourcePropType>)[
+                  this.state.selectedConstructor
+                ]}
+                style={styles.image}
               />
-            ))}
-          </Picker>
-          {this.state.selectedConstructor && (
-            <Image
-              source={(Resources as Record<string, ImageSourcePropType>)[
-                this.state.selectedConstructor
-              ]}
-              style={styles.image}
-            />
-          )}
+            )}
+          </View>
         </View>
+
+        {/* Botón de guardar */}
+        <TouchableOpacity style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Save Dream Team</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -144,8 +200,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  inputContainer: {
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     marginBottom: 20,
+  },
+  inputContainer: {
+    flex: 1,
+    marginRight: 10,
+    marginLeft: 10,
   },
   label: {
     fontSize: 18,
@@ -158,11 +221,22 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   picker: {
-    backgroundColor: '#e0e0e0', 
-    borderRadius: 8, 
-    borderWidth: 1, 
-    borderColor: '#ccc', 
+    backgroundColor: '#e0e0e0',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ccc',
     paddingHorizontal: 10,
-    paddingVertical: 5, 
-  }
+    paddingVertical: 5,
+  },
+  saveButton: {
+    backgroundColor: '#007BFF',
+    borderRadius: 8,
+    padding: 10,
+    marginTop: 20,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
 });
