401d0da910e646d71f5afeffe302e76ec451c27b
pfgubeda <pabloubeda2000@gmail.com>
2023-09-16 13:46:37 +0200

    Added Native Module for adding race as calendar event, includes race name, location, date and time adapted for Timezone

---
+++ ios/PablosF1/PablosCalendarManager.h
@@ -0,0 +1,16 @@
+//
+//  PablosCalendarManager.h
+//  PablosF1
+//
+//  Created by Pablo Fernandez Gonzalez on 16/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PablosCalendarManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/PablosF1/PablosCalendarManager.m
@@ -0,0 +1,12 @@
+//
+//  PablosCalendarManager.m
+//  PablosF1
+//
+//  Created by Pablo Fernandez Gonzalez on 16/9/23.
+//
+
+#import "PablosCalendarManager.h"
+
+@implementation PablosCalendarManager
+
+@end
